a9ce6c250db78ff74f5bcf932eedb6efb1500f60
pavan <pavantejan2000@gmail.com>
2023-10-12 13:29:46 +0530

    small change1

--- movie-booking-ui/src/app/user-components/login/login.component.ts
+++ movie-booking-ui/src/app/user-components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserServiceService } from 'src/app/service/user-service.service';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
+  constructor(
+    private UserService:UserServiceService,
+
+  ){}
   
   ngOnInit(): void {
     throw new Error('Method not implemented.');
@@ -15,7 +21,9 @@ export class LoginComponent implements OnInit {
   username = "";
   password = "";
 
+
   logIn(){
+
 
   }
 
